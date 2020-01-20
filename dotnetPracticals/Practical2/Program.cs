8d6e79f3d7bd92b82854d952b2599abab22d4319
Jay Moliya <jaymoliya33200@gmail.com>
2020-01-20 13:48:37 +0530

    commit

--- dotnetPracticals/Practical2/Program.cs
+++ dotnetPracticals/Practical2/Program.cs
@@ -11,14 +11,17 @@ namespace Practical2
         static void Main(string[] args)
         {
             String name, country;
-
+           
             do{
             Console.WriteLine("Hello,Enter your Name:");
             name = Console.ReadLine();
             }while(!(Regex.IsMatch(name,"^[a-zA-Z]+$")));
 
+            
+            do{
             Console.WriteLine("Where are you from :");
             country = Console.ReadLine();
+            } while (!(Regex.IsMatch(country, "^[a-zA-Z]+$")));
 
             Console.WriteLine();
             Console.WriteLine("Hi " + name + " from country " + country);
