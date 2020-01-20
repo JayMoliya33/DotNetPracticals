350f8ba59d50939e5f923acc387b5c84d63fdffd
Jay Moliya <jaymoliya33200@gmail.com>
2020-01-20 13:40:30 +0530

    commit

--- dotnetPracticals/Practical2/Program.cs
+++ dotnetPracticals/Practical2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Practical2
 {
@@ -11,8 +12,10 @@ namespace Practical2
         {
             String name, country;
 
+            do{
             Console.WriteLine("Hello,Enter your Name:");
             name = Console.ReadLine();
+            }while(!(Regex.IsMatch(name,"^[a-zA-Z]+$")));
 
             Console.WriteLine("Where are you from :");
             country = Console.ReadLine();
